af72d9c851f196109ed12fc7eb6bde320b210f18
raksantos <rss3@ic.ufal.br>
2020-11-29 01:39:19 -0300

    Finalizando dijkstra em C

--- graph.c
+++ graph.c
@@ -1,100 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "graph.h"
 
-struct adjNode{
-    int dest;
-    struct adjNode* next;
+struct grafo{
+    int eh_ponderado;
+    int nro_vertices;
+    int grau_max;
+    int** arestas;
+    float** pesos;
+    int* grau;
 };
 
-typedef struct adjNode node;
+Grafo *cria_grafo(int nro_vertices, int grau_max, int eh_ponderado){
+    Grafo *gr = (Grafo*) malloc(sizeof(struct grafo));
 
-struct adjList{
-    struct adjNode *head;
-};
+    if(gr != NULL){
+        int i;
+        gr->nro_vertices = nro_vertices;
+        gr->grau_max = grau_max;
+        gr->eh_ponderado = eh_ponderado;
+        gr->grau = (int*) calloc(nro_vertices, sizeof(int));
+        gr->arestas = (int**)malloc(nro_vertices*sizeof(int*));
 
-typedef struct adjList aList;
+        for(i = 0; i < nro_vertices; i++){
+            gr->arestas[i] = (int*)malloc(grau_max*sizeof(int));
 
-struct graph{
-    int V;
-    aList* array;
-};
+            if(gr->eh_ponderado){
+                gr->pesos = (float**) malloc(grau_max*sizeof(float));
 
-typedef struct graph Graph;
-
-node* newAdjListNode(int dest)
-{
-    node* newNode = (node*) malloc(sizeof(node));
-    newNode->dest = dest;
-    newNode->next = NULL;
-
-    return newNode;
-}
-
-Graph* createGraph(int V)
-{
-    Graph* newGraph = (Graph*) malloc(sizeof(Graph));
-    newGraph->V = V;
-
-    newGraph->array = (aList*) malloc(sizeof(aList));
-
-    for(int i = 0; i < V; ++i){
-        newGraph->array[i].head = NULL;
+                for(i = 0; i < nro_vertices; i++){
+                    gr->pesos[i] = (float*)malloc(grau_max*sizeof(float));
+                }
+            }
+        }
     }
 
-    return newGraph;
+    return gr;
 }
 
-void addEdge(Graph* grafo, int vertice_ini, int vertice_fim)
-{
-    node* newNode = newAdjListNode(vertice_fim);
-    newNode->next = grafo->array[vertice_ini].head;
-    grafo->array[vertice_ini].head = newNode;
+void libera_grafo(Grafo *gr){
+    if(gr != NULL){
+        int i;
+        for(i = 0; i < gr->nro_vertices; i++){
+            free(gr->arestas[i]);
+        }
+        free(gr->arestas);
 
-    newNode = newAdjListNode(vertice_ini); 
-    newNode->next = grafo->array[vertice_fim].head; 
-    grafo->array[vertice_fim].head = newNode; 
+        if(gr->eh_ponderado){
+            for(i = 0; i < gr->nro_vertices; i++){
+                free(gr->pesos[i]);
+            }
+            free(gr->pesos);
+        }
+        free(gr->grau);
+        free(gr);
+    }
 }
 
+int insereAresta(Grafo* gr, int origem, int destino, int eh_digrafo, float peso){
+    if(gr == NULL) return 0;
 
-void printGraph(Graph* grafo)
-{
-    int v; 
-    for (v = 0; v < grafo->V; ++v) 
-    { 
-        node* pCrawl = grafo->array[v].head; 
-        printf("\n Adjacency list of vertex %d\n head ", v); 
-        while (pCrawl) 
-        { 
-            printf("-> %d", pCrawl->dest); 
-            pCrawl = pCrawl->next; 
-        } 
-        printf("\n"); 
-    } 
-}
+    if(origem < 0 || origem >= gr->nro_vertices) return 0;
 
-int main(){
+    if(destino < 0 || destino >= gr->nro_vertices) return 0;
 
-    int V, arestas;
+    gr->arestas[origem][gr->grau[origem]] = destino;
 
-    printf('Digite o número de vertices: ');
-    scanf('%d', &V);
-
-    printf('Digite o número de arestas: ');
-    scanf('%d', &arestas);
-
-    Graph* grafo = createGraph(V);
-
-    int vertice1, vertice2;
-
-    while(arestas >= 0){
-        printf('Digite o primeiro vertice: ');
-        scanf('%d', &vertice1);
-
-        printf('Digite o segundo vertice: ');
-        scanf('%d', &vertice2);
-
-        addEdge(grafo, vertice1, vertice2);
+    if(gr->eh_ponderado){
+        gr->pesos[origem][gr->grau[origem]] = peso;
     }
 
-    return 0;
+    gr->grau[origem]++;
+
+    if(eh_digrafo == 0){
+        insereAresta(gr, destino, origem, 1, peso);
+    }
+
+    return 1;
+}
+
+int removeAresta(Grafo* gr, int origem, int destino, int eh_digrafo){
+    if(gr == NULL) return 0;
+
+    if(origem < 0 || origem >= gr->nro_vertices) return 0;
+
+    if(destino < 0 || destino >= gr->nro_vertices) return 0;
+
+    int i = 0;
+
+    while(i < gr->grau[origem] && gr->arestas[origem][i] != destino){
+        i++;
+    }
+
+    if(i == gr->grau[origem]){
+        return 0;
+    }
+
+    gr->grau[origem]--;
+    gr->arestas[origem][i] = gr->arestas[origem][gr->grau[origem]];
+    if(gr->eh_ponderado){
+        gr->pesos[origem][i] = gr->pesos[origem][gr->grau[origem]];
+    }
+
+    if(eh_digrafo == 0){
+        removeAresta(gr, destino, origem, 1);
+    }
+
+    return 1;
+}
+
+void printaGrafo(Grafo *gr, int nro_vertices){
+    int i, j;
+
+    if(gr == NULL) return;
+
+    for(i = 0; i < gr->nro_vertices; i++){
+        printf("%d -> ", i);
+        
+        for(j = 0; j < gr->grau[i]; j++){
+            printf("%d", gr->arestas[i][j]);
+            if(gr->eh_ponderado){
+                printf("[%d]", gr->pesos[i][j]);
+            }
+            if(j + 1 < gr->grau[i]){
+                printf(", ");
+            }
+        }
+        printf("\n");
+    }
 }
--- graph.h
+++ graph.h
@@ -1,60 +1,7 @@
-#include<iostream>
-#include<bits/stdc++.h> 
-using namespace std; 
-  
-void addEdge(vector<int> adj[], int u, int v) 
-{ 
-    adj[u].push_back(v); 
-    adj[v].push_back(u); 
-} 
- 
-void printGraph(vector<int> adj[], int V) 
-{ 
-    for (int v = 0; v < V; ++v) 
-    { 
-        cout << "\n Adjacency list of vertex "
-             << v << "\n head "; 
-        for (auto x : adj[v]) 
-           cout << "-> " << x; 
-        printf("\n"); 
-    } 
-} 
-  
-/*Exemplo de código main:
+typedef struct grafo Grafo;
 
-int main() 
-{ 
-    int V, number_edges;
-    
-    cout << "Digite o numero de vertices: ";
-    
-    cin >> V;
-    
-    vector<int> adj[V]; 
-    
-    cout << "Digite o numero de arestas: ";
-    
-    cin >> number_edges;
-    
-    int inicial_vertice, end_vertice;
-    
-    for(int i = 0; i < number_edges; ++i){
-        cout << "Digite o vertice de entrada: ";
-        cin >> inicial_vertice;
-        cout << "Digite o vertice de saida: ";
-        cin >> end_vertice;
-        
-        addEdge(adj, inicial_vertice, end_vertice);
-    }
-    
-    printGraph(adj, V);
-    
-    Exemplo de grafo: 
-
-    Vertices: 0, 1, 2, 3, 4;
-
-    Arestas: (0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4);
-
-    printGraph(adj, V); 
-    return 0; 
-}*/
+Grafo *cria_grafo(int nro_vertices, int grau_max, int eh_ponderado);
+int removeAresta(Grafo* gr, int origem, int destino, int eh_digrafo);
+int insereAresta(Grafo* gr, int origem, int destino, int eh_digrafo, int peso);
+void libera_grafo(Grafo *gr);
+void printaGrafo(Grafo *gr, int nro_vertices);
